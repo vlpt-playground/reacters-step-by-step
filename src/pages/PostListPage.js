bfc18978d2c032024180f859531d27a6e9e2e57a
velopert <public.velopert@gmail.com>
2019-03-15 21:53:48 +0900

    Completes route initialization, setup style system

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './PostListPage.scss';
 
 /**
  * 여러 포스트 목록을 보여주는 페이지
  */
 const PostListPage = () => {
-  return <div>PostListPage</div>;
+  return <div className="PostListPage">PostListPage</div>;
 };
 
 export default PostListPage;
